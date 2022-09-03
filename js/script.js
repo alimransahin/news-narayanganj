bbb39652acc31d57fd2e8084ffd90b276f78b92a
alimransahin <92633905+alimransahin@users.noreply.github.com>
2022-09-03 07:44:10 +0600

    menu load done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log(this);
+const loadMenu=()=>{
+    const url ='https://openapi.programming-hero.com/api/news/categories';
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayMenu(data.data.news_category));
+}
+const displayMenu=menus=>{
+    menus.forEach(menu => {
+        // console.log(menu.category_name);
+        const menuBar = document.getElementById('menu-bar');
+        const li=document.createElement('li');
+        li.classList.add("ki");
+        li.classList.add('nav-item');
+        li.innerHTML =`
+        <a class="nav-link" href="#">${menu.category_name}</a>
+        `;
+        menuBar.appendChild(li);
+    });
+}
+loadMenu();
