7b90a9eb6039e3e3bb2815cd3757b6fea1f75ea8
alimransahin <92633905+alimransahin@users.noreply.github.com>
2022-09-03 14:54:46 +0600

    blog start

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const loadMenu=()=>{
     const url ='https://openapi.programming-hero.com/api/news/categories';
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayMenu(data.data.news_category));
+    .then(data=>displayMenu(data.data.news_category))
+    .catch(error=>console.log(error));
 }
 const displayMenu=menus=>{
     const menuBar = document.getElementById('menu-bar');
@@ -13,10 +14,11 @@ const displayMenu=menus=>{
         li.classList.add("ki");
         li.classList.add('nav-item');
         li.innerHTML =`
-        <a onclick="loadNews('${menu.category_id}')" class="nav-link" href="#">${menu.category_name}</a>
+        <a onclick="loadNews('${menu.category_id}'),toggleLoader(true)" class="nav-link" href="#">${menu.category_name}</a>
         `;
         menuBar.appendChild(li);
     });
+    ;
 }
 loadMenu();
 
@@ -34,6 +36,16 @@ const loadNews =async(id)=>{
 const displayNews=allNews=>{
     const allNewsDiv= document.getElementById('all-news');
     allNewsDiv.innerHTML="";
+    const newsStatus = document.getElementById('news-status');
+    if (allNews.length === 0) {
+        newsStatus.innerText = "No news Found. Please try again another category";
+        newsStatus.classList.remove('d-none');
+
+    }
+    else {
+        newsStatus.innerText = `${allNews.length} news Found.`;
+        newsStatus.classList.remove('d-none');
+    }
     allNews.forEach(news=> {
         // console.log(news);
         const newsDiv = document.createElement('div');
@@ -58,14 +70,14 @@ const displayNews=allNews=>{
                                     </div>
                                     <div class="col-md-9 d-flex align-items-center">
                                         <div class="card-body py-0 my-auto">
-                                            <h5 class="card-title m-0" >${news.author ? news.author.name:"author not found"}</h5>
+                                            <h5 class="card-title m-0" >${(news.author.name != "") && (news.author.name != "system") && (news.author.name != null) ? news.author.name : "author not found"}</h5>
                                             <p class="card-text"><small class="text-muted">${news.author ? news.author.published_date: "published time not found"}</small></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    <p class="card-text text-muted fs-5"><i class="fa-solid fa-eye"></i> ${news.total_view}</p>
+                    <p class="card-text text-muted fs-5"><i class="fa-solid fa-eye"></i> ${news.total_view != null ? news.total_view: '0'}</p>
                     <p class="card-text text-muted">
                     <i class="fa-solid fa-star"></i>
                     <i class="fa-solid fa-star"></i>
@@ -82,13 +94,32 @@ const displayNews=allNews=>{
     `;
         allNewsDiv.appendChild(newsDiv);
     });
+    toggleLoader(false);
+
 }
+// loadNews("01");
+// loader
+const loader = document.getElementById('loader');
+const toggleLoader = isLoading => {
+    if (isLoading) {
+        loader.classList.remove('d-none');
+    }
+    else {
+        loader.classList.add('d-none');
+    }
+}
+// modal load
 const loadDetailsNews=async (id)=>{
     const url = (`https://openapi.programming-hero.com/api/news/${id}`);
-    // console.log(url);
-    const res = await fetch(url);
-    const data = await res.json();
-    displayDetailsNews(data.data[0]);
+    console.log(url);
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayDetailsNews(data[0]);
+    }
+    catch(error){
+        console.log(error);
+    }
     // console.log(data.data[0]);
 }
 const displayDetailsNews=news=>{
@@ -105,7 +136,7 @@ const displayDetailsNews=news=>{
         <div class="d-flex justify-content-between align-item-center">
             <div>
             <img style="max-width:50px" class="rounded-circle me-2" src="${news.author ? news.author.img : "image not found"}">
-            <span class="my-auto">${news.author ? news.author.name : "author not found"}</span>
+            <span class="my-auto">${(news.author.name != "") && (news.author.name != "system") && (news.author.name != null) ? news.author.name : "author not found"}</</span>
             </div>
             <p class="card-text my-auto"><small class="text-muted">Last updated: ${news.author ? news.author.published_date : "published time not found"}</small></p>
         </div>
